cf65ea45e1c0c0317b88ad1dc411d3cde20233ee
yali-hzy <yali-hzy@qq.com>
2025-03-21 19:53:58 +0800

    my solution for kalloctest

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  char name[10];
+  for(int i = 0; i < NCPU; i++){
+    snprintf(name, 10, "kmem%d", i);
+    initlock(&kmem[i].lock, name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +60,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +78,40 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  int cpu = cpuid();
+  pop_off();
+
+  acquire(&kmem[cpu].lock);
+  r = kmem[cpu].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu].freelist = r->next;
+  else{
+    for(int i = 0; i < NCPU; i++){
+      if(i == cpu)
+        continue;
+      acquire(&kmem[i].lock);
+      if(kmem[i].freelist == 0){
+        release(&kmem[i].lock);
+        continue;
+      }
+      struct run *tmp = kmem[i].freelist;
+      for(int j = 0; j < 512; j++){
+        if(tmp->next)
+          tmp = tmp->next;
+        else
+          break;
+      }
+      kmem[cpu].freelist = kmem[i].freelist;
+      kmem[i].freelist = tmp->next;
+      tmp->next = 0;
+      r = kmem[cpu].freelist;
+      kmem[cpu].freelist = r->next;
+      release(&kmem[i].lock);
+      break;
+    }
+  }
+  release(&kmem[cpu].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
